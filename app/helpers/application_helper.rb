5247b85706155cf858d11d09275a8374f431684c
CIT Student <citstudent@Bueno.local>
2015-06-22 23:08:06 +1000

    Added mentor views, edit and delete methods and admin nav buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,8 @@ module ApplicationHelper
     if current_user.present? && current_user.profile.admin?
       nav += '<li>' + link_to('All posts', '/posts')
       nav += '<li>' + link_to('New post', '/posts/new')
+      nav += '<li>' + link_to('All mentors', '/mentors')
+      nav += '<li>' + link_to('New mentor', '/mentors/new')
     end
-
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,7 @@
 Rails.application.routes.draw do
   
 resources :posts
+resources :mentors
 
   devise_for :users, controllers: { registrations: "registrations" }
   root to: 'pages#home'
