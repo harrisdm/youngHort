d6b6f47a65a3cd42e3709597caaed87f9e129366
Mark Harris <harrisdm@hotmail.com>
2015-06-23 11:03:14 +1000

    Fix bug with signup - typo

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,21 +1,21 @@
-aclass RegistrationsController < Devise::RegistrationsController
+class RegistrationsController < Devise::RegistrationsController
  
   def new
     build_resource({})
     self.resource.profile = Profile.new
     respond_with self.resource
   end
- 
+
   def create
     super
   end
- 
- 
+
+
   private
- 
+
   def sign_up_params
     allow = [:email, :password, :password_confirmation, [profile_attributes: [:name, :age, :place_of_study, :course_of_study]]]
     params.require(:user).permit(allow)
   end
- 
+
 end
