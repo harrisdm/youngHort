ba427aa4984528ed7217aab2699298086679a844
CIT Student <citstudent@Bueno.local>
2015-06-25 16:46:01 +1000

    added more leaves. All seems to be working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -58,13 +58,15 @@ $(document).ready(function() {
   //Tweenstuff
   ////////////
 
-  var leafLarge = $('#leafLarge');
+  var leafLarge1 = $('#leafLarge1');
+  var leafLarge2 = $('#leafLarge2');
+  var leafLarge3 = $('#leafLarge3');
 
-  var leafFloat = function(rtl){
+  var leafFloat1 = function(rtl){
     var speed = Math.random() * 5
     var rotation = Math.random() * 800
     var top = Math.random() * $('html').height();
-    leafLarge.css('rotation',  '0deg')
+    leafLarge1.css('rotation',  '0deg')
     if(rtl){
       var left = $('html').width() + 360;
       } else{
@@ -74,7 +76,7 @@ $(document).ready(function() {
     console.log('left', left)
     console.log('rotation', rotation)
     console.log('speed', speed)
-  TweenMax.to(leafLarge, 10 + speed, {
+  TweenMax.to(leafLarge1, 10 + speed, {
     // repeat: -1,
     // yoyo:true,
     ease:Sine.easeInOut,
@@ -83,12 +85,70 @@ $(document).ready(function() {
     rotation: rotation,
     onComplete: function(){
       rtl = (rtl) ? false : true;
-      leafFloat(rtl);
+      leafFloat1(rtl);
     }
     });
   }
 
-  leafFloat(true);
+    var leafFloat2 = function(rtl){
+    var speed = Math.random() * 5
+    var rotation = Math.random() * 800
+    var top = Math.random() * $('html').height();
+    leafLarge2.css('rotation',  '0deg')
+    if(rtl){
+      var left = $('html').width() + 360;
+      } else{
+        var left = -360;
+      }
+    console.log('top', top)
+    console.log('left', left)
+    console.log('rotation', rotation)
+    console.log('speed', speed)
+  TweenMax.to(leafLarge2, 10 + speed, {
+    // repeat: -1,
+    // yoyo:true,
+    ease:Sine.easeInOut,
+    left:left,
+    top: top,
+    rotation: rotation,
+    onComplete: function(){
+      rtl = (rtl) ? false : true;
+      leafFloat2(rtl);
+    }
+    });
+  }
+
+    var leafFloat3 = function(rtl){
+    var speed = Math.random() * 5
+    var rotation = Math.random() * 800
+    var top = Math.random() * $('html').height();
+    leafLarge3.css('rotation',  '0deg')
+    if(rtl){
+      var left = $('html').width() + 360;
+      } else{
+        var left = -360;
+      }
+    console.log('top', top)
+    console.log('left', left)
+    console.log('rotation', rotation)
+    console.log('speed', speed)
+  TweenMax.to(leafLarge3, 10 + speed, {
+    // repeat: -1,
+    // yoyo:true,
+    ease:Sine.easeInOut,
+    left:left,
+    top: top,
+    rotation: rotation,
+    onComplete: function(){
+      rtl = (rtl) ? false : true;
+      leafFloat3(rtl);
+    }
+    });
+  }
+
+  leafFloat1(true);
+  leafFloat2(true);
+  leafFloat3(true);
 
   ////////////
   //Tweenstuff
